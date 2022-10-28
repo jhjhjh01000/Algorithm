c1cc99bc780e5424dc4d56428258bbef1a146257
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2022-10-28 16:50:41 +0900

    Delete 프로그래머스/lv0/120831. 짝수의 합 directory

--- 프로그래머스/lv0/120831. 짝수의 합/짝수의 합.js
+++
@@ -1,9 +0,0 @@
-function solution(n) {
-    let sumEvenNum = 0;
-    for (i = 0; i <= n; i++) {
-        if (i % 2 === 0) {
-            sumEvenNum += i;
-        }
-    }
-    return sumEvenNum;
-}
