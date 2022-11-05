a62448ef66cfdac2c285b76efa0046189964984a
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2022-11-05 14:30:25 +0900

    [level 0] Title: 짝수 홀수 개수, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
+++ 프로그래머스/lv0/120824. 짝수 홀수 개수/짝수 홀수 개수.js
@@ -1,15 +1,14 @@
 function solution(num_list) {
-    var answer = [];
+    var answer = [0,0];
     let even = 0;
     let odd = 0;
     for (let i = 0; i < num_list.length; i++) {
         if (num_list[i] % 2 === 0) {
-            even += 1;
+            even++
             answer[0] = even;
-            answer[1] = odd;
+            
         } else {
-            odd += 1;
-            answer[0] = even;
+            odd++
             answer[1] = odd;
         }
     }
