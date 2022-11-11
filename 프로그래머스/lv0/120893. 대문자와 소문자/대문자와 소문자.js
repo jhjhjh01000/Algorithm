2cf063cb5bab14cbb425faaa5e37f6511221e269
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2022-11-11 23:40:30 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.42 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.js
@@ -2,8 +2,7 @@ function solution(my_string) {
     var answer = '';
     let array = my_string.split('');
     for (let i in array) {
-        if (array[i] === array[i].toUpperCase() &&
-           array[i] !== array[i].toLowerCase()) {
+        if (array[i] === array[i].toUpperCase()) {
             answer += array[i].toLowerCase();
         } else
             answer += array[i].toUpperCase();
