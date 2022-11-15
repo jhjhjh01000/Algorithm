2a74d34750410a777a1e93471c45f0ed527d8c94
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2022-11-16 02:18:40 +0900

    [level 0] Title: 인덱스 바꾸기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120895. 인덱스 바꾸기/인덱스 바꾸기.js
@@ -0,0 +1,7 @@
+function solution(my_string, num1, num2) {
+    var answer = '';
+    let array = my_string.split('');
+    [array[num1], array[num2]] = [array[num2], array[num1]];
+    
+    return array.join('');
+}
