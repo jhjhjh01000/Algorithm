e44824421de3f27877b9513c65651144aae860fb
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2022-11-15 22:14:51 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.js
@@ -1,13 +1,12 @@
 function solution(n) {
     var answer = 0;
-    if (6 % n === 0) {
-        answer = Math.floor(6 / n);
-    }
-    for (let i = 1; i < n+1; i++) {
+    let i = 1;
+    while (true) {
         if ((i * 6) % n === 0) {
             answer = i;
             break;
-        }
-    }    
+        } else
+            i++;
+    }
     return answer;
 }
