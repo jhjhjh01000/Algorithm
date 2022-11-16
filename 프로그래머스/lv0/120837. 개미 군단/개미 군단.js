76fa2cd4a3f250a2e7f288ca2afe032b44603fb2
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2022-11-16 23:42:13 +0900

    [level 0] Title: 개미 군단, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.js
@@ -1,6 +1,4 @@
 function solution(hp) {
-    var answer = 0;
-    
-    answer = Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + Math.floor((hp % 5) % 3 / 1) 
-    return answer;
+        
+    return Math.floor(hp / 5) + Math.floor((hp % 5) / 3) + Math.floor((hp % 5) % 3 / 1);
 }
