9c03ef9a68ede2daebf073bcb4bbee573f216112
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2022-12-18 18:16:47 +0900

    [level 0] Title: 팩토리얼, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.js
@@ -0,0 +1,14 @@
+function solution(n) {
+    var answer = 1;
+    
+    for (let i = 1; i <= n; i++) {
+        answer *= i;
+        
+        if (answer === n) {
+            return i;
+        }else if (answer > n) {
+            return  i - 1;
+        }
+    }
+    return answer;
+}
