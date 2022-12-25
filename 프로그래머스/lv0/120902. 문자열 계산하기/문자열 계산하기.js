4802b1a68432a8a992d86cd2381920d4b9b8e5a9
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2022-12-26 00:53:15 +0900

    [level 0] Title: 문자열 계산하기, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120902. 문자열 계산하기/문자열 계산하기.js
@@ -0,0 +1,13 @@
+function solution(my_string) {
+    my_string = my_string.split(' ')
+    let answer = parseInt(my_string[0]);
+    
+    for (let i = 1; i < my_string.length; i++) {
+        if (my_string[i] === '+') {
+            answer += parseInt(my_string[i+1]);
+        }else if (my_string[i] === '-') {
+            answer -= parseInt(my_string[i+1]);
+        }
+    }
+    return answer;
+}
