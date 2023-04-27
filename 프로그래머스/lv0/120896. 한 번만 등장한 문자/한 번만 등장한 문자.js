fb72212586e27045b6b2348d95af0d82b0419997
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2023-04-27 19:32:54 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
+++ 프로그래머스/lv0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.js
@@ -1,9 +1,8 @@
 function solution(s) {
-    let answer = '';
     const sortArr = s.split('').sort();
     return sortArr.filter((value,index) => sortArr.indexOf(value) === index && sortArr.lastIndexOf(value) === index).join('');
     
     
     
-    return answer;
+    
 }
