b6317c03f3c3ce38bdf0f622a155fb7cb1fe30b4
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2023-08-03 08:47:17 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,8 @@
+function solution(n) {
+    let answer = [];
+    
+    for (let i = 0; i < n; i++) {
+        i % 2 !== 0 ? answer.push("박") : answer.push("수");
+    }
+    return answer.join('');
+}
