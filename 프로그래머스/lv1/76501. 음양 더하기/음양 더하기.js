34be0e150ca285bef3e48bae065a0ff4164e2274
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2023-08-03 07:42:49 +0900

    [level 1] Title: 음양 더하기, Time: 0.25 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
+++ 프로그래머스/lv1/76501. 음양 더하기/음양 더하기.js
@@ -4,8 +4,8 @@ function solution(absolutes, signs) {
     
     for (let i = 0; i < signs.length; i++) {
         signs[i] === false ? 
-            arrForAnswer.push(Number(absolutes[i]) * -1) : 
-        arrForAnswer.push(Number(absolutes[i]));
+            arrForAnswer.push((absolutes[i]) * -1) : 
+        arrForAnswer.push((absolutes[i]));
     }
     
     arrForAnswer.forEach((el) => answer += el);
