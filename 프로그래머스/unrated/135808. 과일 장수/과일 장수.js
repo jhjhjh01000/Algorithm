99eba97a6fa577342bc18fa4b83573857e40a471
jihwan <104141515+jhjhjh01000@users.noreply.github.com>
2023-09-04 13:29:05 +0900

    [unrated] Title: 과일 장수, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -1,15 +1,14 @@
 function solution(k, m, score) {
     let answer = 0;
     let arr = [];
-    let appleBox = Math.floor(score.length / m)
+    let appleBox = Math.floor(score.length / m);
     score = score.sort((a, b) => b - a);
     
     for (let i = 0; i < appleBox; i++) {
-        arr.push(score.slice(i * m, (i + 1) * m))    
+        arr.push(score.slice(i * m, (i + 1) * m));
     }
     
-    answer = arr.map((item) => item.sort((a, b) => a - b)[0] * m)
-    answer = answer.reduce((acc, cur) => acc + cur, 0)
+    answer = arr.map((item) => item.sort((a, b) => a - b)[0] * m).reduce((acc, cur) => acc + cur, 0);
     
     return answer;
 }
